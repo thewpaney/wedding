176daf9d680b38816590eefbc2c0a898181c2620
Ubuntu <ubuntu@ip-172-31-24-95.us-west-2.compute.internal>
2018-01-06 05:15:18 +0000

    Unicorn config and an index page (same as test)

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+
+require File.expand_path '../main.rb', __FILE__
+
+run WeddingApp
--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require './db.rb'
+require 'rubygems'
 require 'sinatra'
 require 'mongoid'
 require 'haml'
@@ -7,25 +7,39 @@ valid_actions = ["rsvp", "ceremony", "reception", "test"]
 
 Mongoid.load!("mongoid.yml")
 
-get '/' do
-  haml :index
+class RSVP
+  include Mongoid::Document
+  store_in collection: "RSVP", database: "wedding"
+
+  field :first, type: String
+  field :last, type: String
+  field :result, type: String
+  field :diet, type: String
 end
 
-get '/*' do |action|
-  if valid_actions.include?(action)
-    status 200
-    haml action.to_sym
-  else
-    status 404
-    haml :err_404
+class WeddingApp < Sinatra::Base
+
+  get '/' do
+    haml :index
   end
-end
+  
+  get '/*' do |action|
+    if valid_actions.include?(action)
+      status 200
+      haml action.to_sym
+    else
+      status 404
+      haml :err_404
+    end
+  end
+  
+  post '/rsvp' do
+    r = RSVP.new
+    r.first = params['first']
+    r.last = params['last']
+    r.result = params['RSVP']
+    r.diet = params['diet']
+    r.save!
+  end
 
-post '/rsvp' do
-  r = RSVP.new
-  r.first = params['first']
-  r.last = params['last']
-  r.result = params['RSVP']
-  r.diet = params['diet']
-  r.save!
 end
